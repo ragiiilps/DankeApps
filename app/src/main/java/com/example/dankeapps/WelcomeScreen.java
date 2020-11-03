a460f45d7ef1dfdf5a246d8a5bdc2bae4585b9d2
NonID <younzb@users.github.com>
2020-11-03 21:55:08 +0700

    initial commit

---
+++ app/src/main/java/com/example/dankeapps/WelcomeScreen.java
@@ -0,0 +1,54 @@
+package com.example.dankeapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    ImageView welcomeLogo;
+    TextView welcomeText;
+    Button loginWelcome, registerWelcome;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        welcomeLogo = findViewById(R.id.logo_welcome);
+        welcomeText = findViewById(R.id.text_welcome);
+        loginWelcome = findViewById(R.id.login_welcome);
+        registerWelcome = findViewById(R.id.register_welcome);
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        loginWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
+        registerWelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+            }
+        });
+
+
+    }
+}
